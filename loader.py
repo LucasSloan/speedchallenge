b93cfa0fae86e7f4848713403870245deb9d7e19
Lucas Sloan <lucas.sloan@gmail.com>
2020-02-18 17:45:23 -0800

    Add cutout regularization.

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 image_feature_description = {
     'frame_one': tf.io.FixedLenFeature([], tf.string),
@@ -21,6 +22,7 @@ def decode_and_process_frame(frame, mirror, training):
         image = tf.image.random_saturation(image, 0.6, 1.6)
         image = tf.image.random_brightness(image, 0.05)
         image = tf.image.random_contrast(image, 0.7, 1.3)
+        image = cutout(image, 40, 0)
     if mirror:
         image = tf.image.flip_left_right(image)
 
@@ -28,6 +30,53 @@ def decode_and_process_frame(frame, mirror, training):
     image = tf.image.per_image_standardization(image)
 
     return image
+
+def cutout(image, pad_size, replace=0):
+  """Apply cutout (https://arxiv.org/abs/1708.04552) to image.
+  This operation applies a (2*pad_size x 2*pad_size) mask of zeros to
+  a random location within `img`. The pixel values filled in will be of the
+  value `replace`. The located where the mask will be applied is randomly
+  chosen uniformly over the whole image.
+  Args:
+    image: An image Tensor of type uint8.
+    pad_size: Specifies how big the zero mask that will be generated is that
+      is applied to the image. The mask will be of size
+      (2*pad_size x 2*pad_size).
+    replace: What pixel value to fill in the image in the area that has
+      the cutout mask applied to it.
+  Returns:
+    An image Tensor that is of type uint8.
+  """
+  image_height = tf.shape(image)[0]
+  image_width = tf.shape(image)[1]
+
+  # Sample the center location in the image where the zero mask will be applied.
+  cutout_center_height = tf.random_uniform(
+      shape=[], minval=0, maxval=image_height,
+      dtype=tf.int32)
+
+  cutout_center_width = tf.random_uniform(
+      shape=[], minval=0, maxval=image_width,
+      dtype=tf.int32)
+
+  lower_pad = tf.maximum(0, cutout_center_height - pad_size)
+  upper_pad = tf.maximum(0, image_height - cutout_center_height - pad_size)
+  left_pad = tf.maximum(0, cutout_center_width - pad_size)
+  right_pad = tf.maximum(0, image_width - cutout_center_width - pad_size)
+
+  cutout_shape = [image_height - (lower_pad + upper_pad),
+                  image_width - (left_pad + right_pad)]
+  padding_dims = [[lower_pad, upper_pad], [left_pad, right_pad]]
+  mask = tf.pad(
+      tf.zeros(cutout_shape, dtype=image.dtype),
+      padding_dims, constant_values=1)
+  mask = tf.expand_dims(mask, -1)
+  mask = tf.tile(mask, [1, 1, 3])
+  image = tf.where(
+      tf.equal(mask, 0),
+      tf.ones_like(image, dtype=image.dtype) * replace,
+      image)
+  return image
 
 def parse_record(tfrecord, training):
     proto = tf.io.parse_single_example(tfrecord, image_feature_description)
